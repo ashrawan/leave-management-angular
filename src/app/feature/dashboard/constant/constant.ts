201073924cb7c6280a3c3493f5ff149214150ad1
ashrawan <shrawan.adhikari.31@gmail.com>
2018-11-28 11:13:11 +0545

    api url added in environvent variable

--- src/app/feature/dashboard/constant/constant.ts
+++ src/app/feature/dashboard/constant/constant.ts
@@ -1,5 +1,6 @@
+import { environment } from './../../../../environments/environment';
+
 export class Constant {
 
-    public static API_ENDPOINT = 'http://127.0.0.1:8080/api';
-    // public static API_ENDPOINT = 'https://pleavemanage.herokuapp.com/api';
+    public static API_ENDPOINT = environment.apiUrl;
 }
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,4 @@
 export const environment = {
-  production: true
+  production: true,
+  apiUrl: 'https://pleavemanage.herokuapp.com/api',
 };
