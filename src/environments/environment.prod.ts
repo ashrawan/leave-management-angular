61cca1c62bb0f4cd2933786932be9a122ae472a3
ashrawan <shrawan.adhikari.31@gmail.com>
2019-01-03 21:18:44 +0545

    fixed on datepicker ui

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://pleavemanage.herokuapp.com/api',
+  apiUrl: 'https://employeeleave.herokuapp.com/api',
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,10 +1,11 @@
+
 // This file can be replaced during build by using the `fileReplacements` array.
 // `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
   production: false,
-  apiUrl: 'http://127.0.0.1:8080/api',
+  apiUrl: 'https://employeeleave.herokuapp.com/api',
 };
 
 /*
